94b7503222a575d4c78f9668e4a00ae798dd03b2
Michael Bowen <michael.bowen994@gmail.com>
2023-07-17 17:33:47 +0000

    Adding the initial project to github.

---
+++ foodplanner/src/main/java/com/michaelbowen/foodplanner/FoodplannerApplication.java
@@ -0,0 +1,13 @@
+package com.michaelbowen.foodplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodplannerApplication.class, args);
+	}
+
+}
---
+++ foodplanner/src/test/java/com/michaelbowen/foodplanner/FoodplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelbowen.foodplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
